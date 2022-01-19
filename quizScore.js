ccdad0fe12d5e9d8185bb824f3e98ee222be2bdd
AurelienGEORGES <91632616+AurelienGEORGES@users.noreply.github.com>
2022-01-19 21:21:39 +0100

    Update quizScore.js

--- quizScore.js
+++ quizScore.js
@@ -34,5 +34,5 @@ saveHighScore = (e) => {
     highScores.splice(5);
     //chargement du score dans le local storage
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
+    window.location.assign('index.html');
 };
